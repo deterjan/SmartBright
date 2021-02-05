a839025d4a06a96eb9010fd594dbfcf706db6e08
Manu Blanco Valentín <manuelblancovalentin@users.noreply.github.com>
2021-02-05 17:21:50 -0600

    SeekBar Test

--- app/src/main/java/com/example/smartbright/MainActivity.java
+++ app/src/main/java/com/example/smartbright/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.Manifest;
 import android.app.AppOpsManager;
@@ -29,6 +30,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+
 public class MainActivity extends AppCompatActivity {
 
     final public static String TAG = "SmartBrightMainActiviy";
@@ -38,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
     boolean mBound = false;
 
     Random r;
+
+
+    //Variable to store brightness value
+    private int brightness;
+    //Content resolver used as a handle to the system's settings
+    private ContentResolver cResolver;
+    //Window object, that will store a reference to the current window
+    private Window window;
+
+    // SeekBar to adjust brightness
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +65,65 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG , "Getting all permissions");
             getAllPermissions();
         }
+
+        // Get seekBar ID
+        seekBar = findViewById(R.id.seekBar);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(getApplicationContext())) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+
+            }
+        }
+
+        //Get the content resolver
+        cResolver = getContentResolver();
+
+        //Get the current window
+        window = getWindow();
+
+        try
+        {
+            // To handle the auto
+            Settings.System.putInt(cResolver,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+            //Get the current system brightness
+            brightness = Settings.System.getInt(cResolver, Settings.System.SCREEN_BRIGHTNESS);
+        }
+        catch (Settings.SettingNotFoundException e)
+        {
+            //Throw an error case it couldn't be retrieved
+            Log.e("Error", "Cannot access system brightness");
+            e.printStackTrace();
+        }
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                brightness = progress;
+                //Set the system brightness using the brightness variable value
+                Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+                //Get the current window attributes
+                WindowManager.LayoutParams layoutpars = window.getAttributes();
+                //Set the brightness of this window
+                layoutpars.screenBrightness = brightness / (float)255;
+                //Apply attribute changes to this window
+                window.setAttributes(layoutpars);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
         // Start application
         Intent intent = new Intent(MainActivity.this, ServiceClassPhone.class);
